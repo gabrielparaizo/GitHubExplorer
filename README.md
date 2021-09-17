9357711ed44bc265ad459abe2d3bd79b8e11a467
Gabriel Lopes <gabriellopees>
2021-09-17 14:25:20 -0300

    :tada: Project creation
    :memo: docs: Add README.m, Babel and Webpack

---
+++ README.md
@@ -0,0 +1,11 @@
+# **GitHub Explorer**
+
+## **Sobre**
+GitHub Explorer é o primeiro projeto criado no Bootcamp Ignite da [Rocketseat](rocketseat.com.br) na trilha de ReactJS
+
+## **Conteúdo**
+* **[Tecnologias](#tecnologias-computer)**
+
+## **Tecnologias :computer:**
+* Primeira tecnoligia
+* Segunda Tecnologia
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
